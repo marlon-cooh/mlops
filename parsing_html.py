c03d375b3f51a15c60fb049c6f4fab9e944df30d
Marlon Marin <marlonmariin97@outlook.com>
2025-09-21 20:34:48 -0500

    (build) Refine pipeline built in parsing_html.py

--- parsing_html.py
+++ parsing_html.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import pandas as pd #type:ignore
+import os
 import logging
 from io import StringIO
 
@@ -31,7 +32,7 @@ def retrieve_student_info(html_file) -> list:
     
     return info_students
 
-def retrieve_student_table(html_file) -> pd.DataFrame:
+def retrieve_student_table(html_file, table_id) -> pd.DataFrame:
     """ 
         Retrieve student table from HTML file.
         Args:
@@ -46,7 +47,8 @@ def retrieve_student_table(html_file) -> pd.DataFrame:
     soup = BeautifulSoup(content, "html.parser")
     
     # Reading table and converting to DataFrame.
-    studs_table = soup.find("table", id="notas_div_tabla_estudiantes")
+    ## "notas_div_tabla_estudiantes"
+    studs_table = soup.find("table", id=table_id)
     io = StringIO(str(studs_table))
     try:
         [dfs] = pd.read_html(io, flavor='bs4')
@@ -74,10 +76,55 @@ def retrieve_grades_table(html_file) -> pd.DataFrame:
 
 if __name__ == "__main__":
     # Grades files
-    data_10_1 = "10_1_grades.html"
+    data_10_1 = "./grades_html/10_1_2grades.html"
     
-    studs_info = retrieve_student_info(data_10_1)
+    try:
+        assert os.path.exists(data_10_1), f"File {data_10_1} does not exist."
+    except AssertionError as e:
+        logger.error(e)
+        raise
+    
+    # Prettify and save the HTML file (optional)
+    with open(data_10_1, "r", encoding="utf-8") as file:
+        content = file.read()
+    soup = BeautifulSoup(content, "html.parser")
+    pretty_html = soup.prettify()
+    with open(data_10_1, "w", encoding="utf-8") as file:
+        file.write(pretty_html)
+        
+    # Generate DataFrames.
+    studs_info = retrieve_student_info("./grades_html/10_1_1grades.html")
     studs_table = retrieve_grades_table(data_10_1)
+    grades_table = retrieve_student_table(data_10_1, "notas_tabla_competencias")
 
     studs_table.index = studs_info
-    logger.info(studs_table)
+    
+    new_cols = [
+       'Competencia',
+       'CONOCER', 
+       'HACER',
+       'SER', 
+       'CONVIVIR',
+       'Subtotal NIVEL', 
+       'Nota P1', 
+       'Rec P1',
+       'Nota P2', 
+       'Rec P2',
+       'Nota P3',
+       'Rec P3', 
+       'Nota PF', 
+       'Rec PF',
+       'OBS  1',
+       'OBS  2',
+       'OBS  3',
+       'OBS  4',
+       'OBS  5',
+       'LLEGADA TARDE', 
+       'INASISTENCIA JUSTIFICADA',
+       'INASISTENCIA INJUSTIFICADA',
+       'PERMISO', 
+       'RETARDO']
+    studs_table.columns = new_cols
+    
+    # Redirect this info into a .pkl file.
+    print(studs_table)
