ca1e4d7eb84bec907f65034aa12b6ce43aa618c8
Marlon Marin <marlonmariin97@outlook.com>
2025-11-17 12:36:39 -0500

    (new) 	utils/log_data.py > script to arrange logger and import data functions. (build) registry.py	  > recursion for client promote model function. (build) tracking.py	  > Script was arranged to handle several experiments at same time and tracking them in mlflow. (delete) parsing_html.log, model_comparison.log, utils/parsing_html.log > Unused files.

--- registry.py
+++ registry.py
@@ -1,53 +1,20 @@
 import mlflow #type:ignore
-import logging
 from scipy.sparse._csr import csr_matrix #type:ignore
 from mlflow.tracking import MlflowClient #type:ignore
 from datetime import datetime
-from logging.handlers import RotatingFileHandler
 from tracking import get_train_data, DATA_PATH, reducing_dimensionality
 from sklearn.metrics import accuracy_score, f1_score #type:ignore
-
+from utils.log_data import logger_setup
 
 # ---- 1. Configuration ----
 EXPERIMENT_NAME = "LaHolandaPerformance"
 MODEL_NAME = "jurgendhilfe-weltweit"
+MLFLOW_TRACKING_URI = "http://127.0.0.1:5000"
 
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow.set_experiment(EXPERIMENT_NAME)
 
 # --- 2. Functions ---
-def logger_setup(logger_name: str) -> None:
-    """Sets up logger for the module."""
-    
-    # Main logger.
-    logger = logging.getLogger(logger_name)
-
-    if logger.handlers:
-        return logger
-    
-    logger.setLevel(level=logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    
-    # Console -> INFO+
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)   
-    console_handler.setFormatter(formatter)
-
-    # File -> DEBUG+
-    file_handler = RotatingFileHandler(
-        "model_registry.log",
-        maxBytes=1024*1024,  # 1MB
-        backupCount=5
-    )
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-    
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-    
-    return logger
-
-
 def test_model_from_mlflow(model_name:str, stage:str, X_test:csr_matrix, y_test) -> None:
     """this function tests a model from mlflow
     Args:
@@ -65,14 +32,16 @@ def test_model_from_mlflow(model_name:str, stage:str, X_test:csr_matrix, y_test)
     acc_score = round(f1_score(y_test, y_pred, average='weighted'), 2)
     return {"f1_score": acc_score}
 
-if __name__ == "__main__":
-
-    X_train, X_test, y_train, y_test = get_train_data(DATA_PATH)
-    X_train_pca, X_test_pca = reducing_dimensionality(X_train, X_test)
-    logger = logger_setup("ModelRegistry.log")
-    
-    client = MlflowClient(tracking_uri="sqlite:///mlflow.db")
-    exp = client.get_experiment_by_name(EXPERIMENT_NAME)
+def promote_model(tracking_uri:str, exp_name:str, model_name:str, version:int, stage:int):
+    """
+        this function promotes the best model to production.
+    Args:
+        * tracking_uri (str): mlflow tracking uri
+        * exp_name (str): experiment name
+        
+    """
+    client = MlflowClient(tracking_uri=tracking_uri)
+    exp = client.get_experiment_by_name(exp_name)
     runs = client.search_runs(
         experiment_ids=[exp.experiment_id]
     )
@@ -80,29 +49,51 @@ if __name__ == "__main__":
     # Best run based on f1 score
     scores = {r.info.run_id:r.data.metrics['f1 score'] for r in runs}
     best_run_id = max(scores, key=scores.get)
-    logger.info(f"Best run ID: {best_run_id}")
+    logger.info(f"Best run ID: {best_run_id}, F1 Score: {scores[best_run_id]}")
 
     # Registering best model.
     mlflow.register_model(model_uri=f"runs:/{best_run_id}/la_holanda_model",
-        name=MODEL_NAME
+        name=model_name
     )
     latest_versions = client.get_latest_versions(name=MODEL_NAME)
-    for version in latest_versions:
-        logger.info(f"Model: {MODEL_NAME}, Version: {version.version}, Stage: {version.current_stage}")
+    for version_ in latest_versions:
+        logger.info(f"Model: {model_name}, Version: {version_.version}, Stage: {version_.current_stage}")
 
-    # ---- 3. Transitioning model to Production ----
+    # Transitioning model to `stage``
     client.transition_model_version_stage(
-        name=MODEL_NAME,
-        version=3,
-        stage="Production"
+        name=model_name,
+        version=version,
+        stage=stage
     )
 
     client.update_model_version(
-        name=MODEL_NAME, 
-        version=3,
-        description=f"The model version {3} was transitioned to Production on {datetime.today().date()}"
+        name=model_name, 
+        version=version,
+        description=f"The model version {version_.version} was transitioned to Production on {datetime.today().date()}"
     )
-    logger.info(f"Client updated model version description.")
+    
+    return f"The model version {version_.version} was transitioned to Production on {datetime.today().date()}"
+
+if __name__ == "__main__":
+
+    # Parameters.
+    model_version = 9
+    stage = "Production"
+    
+    # Output from get_train_data and reducing_dimensionality/
+    X_train, X_test, y_train, y_test = get_train_data(DATA_PATH)
+    X_train_pca, X_test_pca = reducing_dimensionality(X_train, X_test)
+    logger = logger_setup("ModelRegistry.log")
+    
+    response = promote_model(
+        tracking_uri=MLFLOW_TRACKING_URI, 
+        exp_name=EXPERIMENT_NAME,
+        model_name=MODEL_NAME, 
+        version=model_version, 
+        stage=stage
+    )
+    
+    logger.info(response)
     
     logger.info(f"X_test shape: {X_test.shape}, and PCA transformed shape: {X_test_pca.shape}")
     result = test_model_from_mlflow(model_name=MODEL_NAME, stage="Production", X_test=X_test_pca, y_test=y_test)
--- tracking.py
+++ tracking.py
@@ -1,213 +1,188 @@
 from tqdm import tqdm
-import logging
+import time
 import subprocess
 import pandas as pd #type:ignore
-from logging.handlers import RotatingFileHandler
-
+from utils.log_data import logger_setup, get_train_data, reducing_dimensionality
 
 # sklearn.
-from sklearn.model_selection import train_test_split, GridSearchCV #type:ignore
+from sklearn.model_selection import GridSearchCV #type:ignore
 from sklearn.linear_model import LogisticRegression #type:ignore
 from sklearn.ensemble import RandomForestClassifier #type:ignore
-from sklearn.metrics import classification_report, accuracy_score, f1_score, roc_auc_score#type:ignore
-from imblearn.over_sampling import RandomOverSampler, SMOTEN #type:ignore
-from sklearn.decomposition import PCA #type:ignore
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score #type:ignore
+from imblearn.over_sampling import SMOTEN #type:ignore
 
 # mlflow.
 import mlflow #type:ignore
 from mlflow.models import infer_signature #type:ignore
-from mlflow.tracking import MlflowClient #type:ignore
 
 
-# ---- 1. Model and parameter Grid configuration ----
-MODELS_CONFIG = {
-    'LogisticRegression' : {
-        'model' : LogisticRegression(random_state=42, max_iter=2000),
-        'params' : {
-            'C': [0.001, 0.01, 0.1, 1, 10],
-            'penalty': ['l1', 'l2'],
-            'solver': ['liblinear', 'saga']
-        }
+# ---- 1. Models and experiments ----
+EXPERIMENTS = {
+    "logreg_simple": {
+        "experiment_name": "LaHolanda_LogReg_Simple",
+        "model": LogisticRegression(random_state=42, max_iter=2000),
+        "param_grid": None,
     },
-    'RandomForest' : {
-        'model' : RandomForestClassifier(random_state=42),
-        'params' : {
-            'n_estimators' : [100, 200, 500],
-            'max_depth' : [10, 20, 50, 100],
-            'criterion' : ['gini', 'entropy']
-        }
-    }
+    "logreg_grid": {
+        "experiment_name": "LaHolanda_LogReg_Grid",
+        "model": LogisticRegression(random_state=42, max_iter=2000),
+        "param_grid": {
+            "C": [0.001, 0.01, 0.1, 1, 10],
+            "penalty": ["l1", "l2"],
+            "solver": ["liblinear", "saga"],
+        },
+    },
+    "rf_simple": {
+        "experiment_name": "LaHolanda_RF_Simple",
+        "model": RandomForestClassifier(random_state=42),
+        "param_grid": None,
+    },
+    "rf_grid": {
+        "experiment_name": "LaHolanda_RF_Grid",
+        "model": RandomForestClassifier(random_state=42),
+        "param_grid": {
+            "n_estimators": [100, 200, 500],
+            "max_depth": [10, 20, 50, 100],
+            "criterion": ["gini", "entropy"],
+        },
+    },
+    # later: "logreg_boosting", "naive_bayes", ...
 }
 
-EXPERIMENT_NAME = "LaHolandaPerformance"
 REGISTERED_MODEL_NAME = "jurgendhilfe-weltweit"
 DATA_PATH = "./cleaned_data/grade_summary.parquet"
 
-# # --- 2. Setting Mlflow client ---
-# client = MlflowClient(tracking_uri="sqlite:///mlflow.db")
-# client.create_experiment(name=EXPERIMENT_NAME)
+# --- 2. Setting logger ----
+logger = logger_setup("model_comparison.log")
 
-# --- 3. Setting logger ----
-# Main logger.
-logger = logging.getLogger("ModelComparison")
-logger.setLevel(
-    level=logging.INFO
-)
-# Format for logger.
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-# File handler.
-file_handler = RotatingFileHandler(
-    "model_comparison.log",
-    maxBytes=1024*1024,  # 1MB
-    backupCount=5
-)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
+# --- 3. Loading data ---
+X_train, X_test, y_train, y_test = get_train_data(DATA_PATH, 'band')
 
-# Console handler (only INFO)
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-console_handler.setFormatter(formatter)
-
-# Add handlers to logger
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
-
-# ---- 4. Data Preparation ----
-# Loading data.
-def get_train_data(df_path: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    data = pd.read_parquet(path=df_path)
-    X = data.drop(columns=['band'])
-    y = data['band'].to_numpy().ravel()
-            
-    # Train-test split.
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42, stratify=y
-    )
-
-    return X_train, X_test, y_train, y_test
-
-def reducing_dimensionality(x_train, x_test) -> tuple:
-    """Applies PCA to reduce dimensionality of the data.
-    Args:
-        X_train (pd.DataFrame): training data
-        X_test (pd.DataFrame): test data
-    Returns:
-        tuple: transformed training and test data
-    """
-    pca = PCA(n_components=0.8, random_state=42)
-    X_train_pca = pca.fit_transform(x_train)
-    X_test_pca = pca.transform(x_test)
+# --- 4. Launching mlflow process ---
+def launch_mlflow_server(host:str, port:int) -> None:
+    """Launch a mlflow server and return the process object for later termination.
     
-    return X_train_pca, X_test_pca
-
-def main(host="127.0.0.1", port=5000) -> None:
-    """Main function to run the model comparison experiments."""
-
-    X_train, X_test, y_train, y_test = get_train_data(DATA_PATH)
-    try:        
-        # Opening MLflow server as a subprocess.
-        mlflow_process = subprocess.Popen(
-            [
-                "mlflow", "server",
-                "--backend-store-uri", "sqlite:///mlflow.db",
-                "--default-artifact-root", "./mlruns",
-                "--host", host,
-                "--port", str(port)
-            ]
-        )
-
-        logger.info(f"✅ MLflow server started at http://{host}:{port}")
-        try:
-            client =  MlflowClient(tracking_uri=f"http://{host}:{port}")
-            exp = client.get_experiment_by_name(EXPERIMENT_NAME)
-            if exp and exp.lifecycle_stage == 'deleted':
-                client.restore_experiment(exp.experiment_id)
-                logger.info(f"Restored deleted experiment: {EXPERIMENT_NAME}")
-        except Exception as e:
-            logger.warning(f"Could not restore experiment: {e}")
+    Args:
+        host: Server host address
+        port: Server port number
         
-        # # Applying PCA for dimensionality reduction.
-        # X_train = X_train.astype(int)
-        # pca = PCA(n_components=0.8, random_state=42)
-        # X_train_pca = pca.fit_transform(X_train)
-        # X_test_pca = pca.transform(X_test)
+    Returns:
+        subprocess.Popen: The MLflow server process object
+    """
+    
+    # Opening MLflow server as a subprocess.
+    mlflow_process = subprocess.Popen(
+        [
+            "mlflow", "server",
+            "--backend-store-uri", "sqlite:///mlflow.db",
+            "--default-artifact-root", "./mlruns",
+            "--host", host,
+            "--port", str(port)
+        ]
+    )
+    
+    logger.info(f"✅ MLflow server started at http://{host}:{port}")    
+    return mlflow_process
+    
+def main(timeout:int = 120) -> None:
+    """Main function to run the model comparison experiments.
+    
+        Args:
+        timeout: Timeout in seconds before terminating MLflow server
+    """
+
+    start_time = time.time()
+    try:
         X_train_pca, X_test_pca = reducing_dimensionality(X_train, X_test)
-        
+    
         # Treating imbalance with SMOTEN.
         smoten = SMOTEN(random_state=42)
         X_train_res, y_train_res = smoten.fit_resample(X_train_pca, y_train)
         
         # Finding best model.
-        for model in tqdm(MODELS_CONFIG.keys(), desc="Training models"):
+        for experiment_key, exp in tqdm(EXPERIMENTS.items(), desc="Training models"):
             
-            logger.info(f"Running GridSearchCV for model: {model}")
+            experiment_name = exp["experiment_name"]
             
+            # Check timeout
+            elapsed = time.time() - start_time
+            if elapsed > timeout:
+                logger.warning(f"Timeout ({timeout}s) exceeded. Stopping training.")
+                break
+            logger.info(f"Running experiment for model: {experiment_name}")
+        
             # Calling hyperparam optimizer.
-            gcv = GridSearchCV(
-                estimator=MODELS_CONFIG[model]['model'],
-                param_grid=MODELS_CONFIG[model]['params'],
-                scoring='accuracy',
-                cv=5,
-                n_jobs=-1
-            )
+            param_grid = exp['param_grid']
+            if param_grid:
+                estimator = GridSearchCV(
+                    estimator=exp['model'],
+                    param_grid=param_grid,
+                    scoring='accuracy',
+                    cv=5,
+                    n_jobs=1, # Use 1 job to avoid overloading the system
+                )
+            else:
+                estimator = exp['model']
 
             # Training model.
-            gcv.fit(X_train_res, y_train_res)
-                
+            estimator.fit(X_train_res, y_train_res)
+
             # Predict on the test set.
-            y_pred = gcv.predict(X_test_pca)
+            y_pred = estimator.predict(X_test_pca)
             # y_pred_proba = gcv.predict_proba(X_test_pca)[:, 1]
             
             # Calculate metrics
             acc = accuracy_score(y_test, y_pred) # This is the metric to keep track of.
             f1 = f1_score(y_test, y_pred, average='weighted')
             # roc_score = roc_auc_score(y_test, y_pred_proba)
-            logger.info(f"Best parameters for {model}: {f1}")
+            logger.info(f"Metrics for experiment {experiment_key} were \n * F1 score: {f1} \n * Accuracy: {acc}")
         
-            # MLflow experiment setup.
-            mlflow.set_tracking_uri(uri="sqlite:///mlflow.db")
-            
             # Create a new MLflow Experiment
-            mlflow.set_experiment(EXPERIMENT_NAME)
-            
+            mlflow.set_experiment(experiment_name=experiment_name)
+        
             # Start an MLflow run
             with mlflow.start_run() as run:
-                
+
                 # Log the hyperparams
-                mlflow.log_params(gcv.best_params_)
-                
+                if isinstance(estimator, GridSearchCV):
+                    mlflow.log_params(estimator.best_params_)
+                    logger.info(f"Best params for {experiment_key}: {estimator.best_params_}")
+                else:
+                    mlflow.log_params(estimator.get_params())
+                    logger.info(f"Best params for {experiment_key}: {estimator.get_params()}")
+
                 # Log the loss metric
                 mlflow.log_metric("accuracy", acc)
                 mlflow.log_metric("f1 score", f1)
                 # mlflow.log_metric("roc_auc", roc_score)
-                
+
                 # Infer the model signature
-                signature = infer_signature(X_train_pca, gcv.predict(X_train_pca))
-                
+                signature = infer_signature(X_train_pca, estimator.predict(X_train_pca))
+
                 # Setting a tag that we can use to remind this model.
-                mlflow.set_tag("Training info", f"{MODELS_CONFIG[model]['model']} for La Holanda analysis.")
-                
+                mlflow.set_tag("Training info", f"{exp['model']} for La Holanda analysis.")
+
                 # Log the model, which inherits the parameters and metric
                 model_info = mlflow.sklearn.log_model(
-                    sk_model=gcv,
+                    sk_model=estimator,
                     artifact_path="la_holanda_model",
                     signature=signature,
-                    input_example=X_train.iloc[:5],
-                    registered_model_name=REGISTERED_MODEL_NAME
+                    input_example=X_train_pca[:20],
+                    registered_model_name=REGISTERED_MODEL_NAME # jurgendhilfe-weltweit is the model to optimize and deploy.
                 )
-                
-                logger.info(f"Model {model} logged in MLflow with run ID: {model_info.run_id}")
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-        raise
-    finally:
-        mlflow_process.terminate()
-        mlflow_process.wait(timeout=10)
-        mlflow_process.kill()
 
-    logger.info("Model comparison experiments completed.")
-    logger.info("MLflow server terminated.")
+                logger.info(f"Experiment {experiment_key} logged in MLflow with run ID: {model_info.run_id}")
+
+        logger.info("Model comparison experiments completed.")
+        logger.info("MLflow server terminated.")
     
+    except Exception as e:
+        logger.error(f"An error occurred during model training: {e}")
+        raise
+   
 if __name__ == "__main__":
-    main()
+    
+    # Launch MLflow server.
+    mlflow.set_tracking_uri(uri="sqlite:///mlflow.db")
+    # Run main function with timeout.
+    main(timeout=120)
